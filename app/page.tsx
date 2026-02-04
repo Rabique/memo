5715f6d4ade5f3e283b28b58ddd412b4eb18636d
rabique <oddyseibcm@gmail.com>
2026-02-04 15:06:09 +0900

    preview again

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,7 @@ export default function Home() {
               width={16}
               height={16}
             />
-            Deploy preview
+            Deploy preview again
 
           </a>
           <a
